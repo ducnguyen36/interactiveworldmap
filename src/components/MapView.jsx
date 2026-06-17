75490b0ce14529d60ba71ae90e4e738458f23603
ducnguyen36 <ducnguyen36@gmail.com>
2026-06-17 11:45:02 +0700

    feat: reposition zoom control and replace Leaflet attribution branding

--- src/components/MapView.jsx
+++ src/components/MapView.jsx
@@ -1,4 +1,4 @@
-import { MapContainer } from 'react-leaflet';
+import { MapContainer, ZoomControl, AttributionControl } from 'react-leaflet';
 import { LAYERS } from '../data/layers.js';
 
 export default function MapView({ activeBaseId, activeOverlayIds }) {
@@ -13,8 +13,12 @@ export default function MapView({ activeBaseId, activeOverlayIds }) {
       minZoom={2}
       maxZoom={7}
       worldCopyJump
+      zoomControl={false}
+      attributionControl={false}
       className="h-full w-full"
     >
+      <ZoomControl position="topright" />
+      <AttributionControl position="bottomright" prefix="Natural Earth · Köppen-Geiger · FAO · Wikipedia" />
       {active.map((l) => l.component && <l.component key={l.id} />)}
     </MapContainer>
   );
